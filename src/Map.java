1ca20b9291fe78b4d86bda13110a55a00994ba8a
okquentin <qosterhage@outlook.com>
2023-05-12 19:01:44 -0400

    Map is finalized, all forts are added. Did not add natural landmarks, don't think they're necessary.

--- src/Map.java
+++ src/Map.java
@@ -33,15 +33,36 @@ public class Map {
 	    private boolean crossBlue;
 	    private boolean crossPlatte;
 	    private boolean crossPlatte2;
+		public boolean crossGreen;
+		public boolean crossSnake;
 	    private final int distBR = 130;
 	    private final int distPR = 250;
 	    private final int distPR2 = 430;
+		private final int distGreen = 950;
+		private final int distSnake = 1600;
+		public int distGR = 950;
 
 	    public int distKearny = 260;
 	    public boolean visitKearny;
-	    
-	    public int distAH = 480;  // Ash Hollow
+
+		public int distAH = 480;  // Ash Hollow
 	    public boolean visitAH;
+
+		public int distLaramie = 730;
+		public boolean visitLaramie;
+
+		public int distBridger = 1000;
+		public boolean visitBridger;
+
+		public int distHall = 1200;
+		public boolean visitHall;
+
+		public int distBoise = 1450;
+		public boolean visitBoise;
+
+		public int distWalaWala = 1700;
+		public boolean visitWalaWala;
+
 
 		public int distWV = 1000; // Willamette Valley (END OF GAME)
 		public boolean visitWV;
@@ -58,8 +79,16 @@ public class Map {
 			crossBlue = false;
 			crossPlatte = false;
 			crossPlatte2 = false;
+			crossGreen = false;
+			crossSnake = false;
 			visitKearny = false;
 			visitAH = false;
+			visitLaramie = false;
+			visitBridger = false;
+			visitHall = false;
+			visitBoise = false;
+			visitWalaWala = false;
+			visitWV = false;
 	    }
 	    
 	    /**
@@ -100,14 +129,64 @@ public class Map {
 			   exitTown = false;
 			   fortsPassed++;
 		   }
+
+		   if(distanceTraveled >= distAH && visitAH == false) {
+			visitAH = true;
+			terrain = 3;
+			currTown = "Ash Hollow";
+			exitTown = false;
+			fortsPassed++;
+		}
 		        
-	       if(distanceTraveled >= distAH && visitAH == false) {
-	    	   visitAH = true;
+	       if(distanceTraveled >= distLaramie && visitLaramie == false) {
+	    	   visitLaramie = true;
 			   terrain = 3;
-			   currTown = "Ash Hollow";
+			   currTown = "Fort Laramie";
 			   exitTown = false;
 			   fortsPassed++;
 	       }
+
+		   if(distanceTraveled >= distBridger && visitBridger == false) {
+			visitBridger = true;
+			terrain = 3;
+			currTown = "Fort Bridger";
+			exitTown = false;
+			fortsPassed++;
+		}
+
+		if(distanceTraveled >= distHall && visitHall == false) {
+			visitHall = true;
+			terrain = 3;
+			currTown = "Fort Hall";
+			exitTown = false;
+			fortsPassed++;
+		}
+
+		if(distanceTraveled >= distBoise && visitBoise == false) {
+			visitBoise = true;
+			terrain = 3;
+			currTown = "Fort Boise";
+			exitTown = false;
+			fortsPassed++;
+		}
+
+
+		if(distanceTraveled >= distWalaWala && visitWalaWala == false) {
+			visitWalaWala = true;
+			terrain = 3;
+			currTown = "Fort Walla Walla";
+			exitTown = false;
+			fortsPassed++;
+		}
+
+
+		   if(distanceTraveled >= distWV && visitWV == false) {
+			visitWV = true;
+			terrain = 3;
+			currTown = "Willamette Valley";
+			exitTown = false;
+			fortsPassed++;
+		}
 	       
 	       if(distanceTraveled >= 380 && exitTown != false) {terrain = 1;} 
 	   
@@ -129,5 +208,17 @@ public class Map {
 	    	   currRiver = "Platte River";
 	    	   crossPlatte2 = true;
 	       }
+		   if(distanceTraveled >= distGreen && crossGreen == false) {
+			riverGen();
+			terrain = 2;
+			currRiver = "Green River";
+			crossPlatte2 = true;
+		}
+		if(distanceTraveled >= distSnake && crossSnake == false) {
+			riverGen();
+			terrain = 2;
+			currRiver = "Snake River";
+			crossPlatte2 = true;
+		}
 	    }
 }
